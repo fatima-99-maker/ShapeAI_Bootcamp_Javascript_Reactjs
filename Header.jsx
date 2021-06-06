827428a16c1c8f83b3ec5ec8a71b3d2e0deab783
Fatima Abdul Jabbar <82316692+fatima-99-maker@users.noreply.github.com>
2021-06-06 19:57:02 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1> SuperSafe</h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ Note.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h2> Improving Security With Enhanced Technology</h2>
+      <h1> Four Wheeler Security System & GPS Tracking Device </h1>
+      <p>
+        SuperSafe is a leading car tracking devices manufacturing company in
+        India.We offer GPS device for a car at an affordable price.SuperSafe
+        provides the best solution to protect and secure your vehicle by
+        installing a car tracker app.SuperSafe’s car GPS tracker made a simple
+        and easy way to alert your vehicle from theft.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
